84487fa5b9a54e07e8b8a6bef08cff3acd054f87
abdelaaziz jcweb <islamabdelaaziz@gmail.com>
2022-10-19 22:15:50 +0100

    changer les fichiers register

--- src/Controller/Backend/AccueilController.php
+++ src/Controller/Backend/AccueilController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Backend;
 use App\Entity\Client;
 use App\Entity\Commande;
 use App\Repository\CommandeRepository;
+use App\Repository\CoursRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,82 +19,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     #[Route('/admin', name: 'admin_accueil')]
-    public function adminAccueil(Request $request,EntityManagerInterface $em ): Response
+    public function adminAccueil(Request $request,CoursRepository $coursRepository,EntityManagerInterface $em ): Response
     {
-
-//        $form = $this->container->get('form.factory')->createBuilder()
-//            ->setMethod('POST')
-//            ->add(
-//                'dateDebut',
-//                DateType::class,
-//                [
-//                    'widget' => 'single_text',
-//
-//                ]
-//            )
-//            ->add(
-//                'dateFin',
-//                DateType::class,
-//                [
-//                    'widget' => 'single_text',
-//                ]
-//            )
-//            ->add(
-//                'valider',
-//                SubmitType::class,
-//                array(
-//                    'attr' => array(
-//                        'class' => 'btn btn-success',
-//                    ),
-//                )
-//            )
-//            ->getForm();
-//        $form->handleRequest($request);
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            $dateDebut = $form->getData()['dateDebut'];
-//            $dateFin = $form->getData()['dateFin'];
-//
-//        } else {
-//
-//            $dateDebut = null;
-//            $dateFin = null;
-//        }
-//
-//
-//        $cmdConfirmee = $em->getRepository(Commande::class)->cmdConfirmee($dateDebut, $dateFin);
-//        $nbrCmds = $em->getRepository(Commande::class)->nbrCmds($dateDebut, $dateFin);
-//        $cmdAnnulee= $em->getRepository(Commande::class)->cmdAnnulee($dateDebut, $dateFin);
-//        $cmdsStats=array();
-//        $mois=['janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Decembre'];
-//        $montantByMonthAndYear=array();
-//        for($i = 2022; $i <= 2023; $i++)
-//        {
-//            for($j=1;$j<=12;$j++)
-//            {
-//                $montantByMonthAndYear[$j-1]['mois']=$mois[$j-1];
-//                if( $em->getRepository(Commande::class)->montantByMonthAndYear($j,$i)==null)
-//                $montantByMonthAndYear[$j-1]['montant']=0;
-//                else
-//                    $montantByMonthAndYear[$j-1]['montant'] =$em->getRepository(Commande::class)->montantByMonthAndYear($j,$i);
-//                ;            }
-//            $cmdsStats[$i]=$montantByMonthAndYear;
-//
-//        }
-//
-//        for($i = 1; $i <= 12; $i++)
-//        {
-//            $clientsCmdsStats[$i-1]['mois']=$mois[$i-1];
-//            $clientsCmdsStats[$i-1]['client']=$em->getRepository(Client::class)->clientByMonthAndYear($i,2022);
-//            $clientsCmdsStats[$i-1]['commande']=$em->getRepository(Commande::class)->cmdByMonthAndYear($i,2022);
-//        }
-        return $this->render('backend/accueil/index.html.twig', array(
-//            "form" => $form->createView(),
-//            'nbrClients' => $nbrClients,
-//            'cmdConfirmee' => $cmdConfirmee,
-//            'cmdAnnulee'=>$cmdAnnulee,
-//            'nbrCmds'=>$nbrCmds,
-//            'cmdsStats'=>json_encode($cmdsStats),
-//            'clientsCmdsStats'=>json_encode($clientsCmdsStats),
+       $cours=$coursRepository->findAll();
+        return $this->render('backend/cours/index.html.twig', array(
+           'cours'=>$cours
 
         ));
 
