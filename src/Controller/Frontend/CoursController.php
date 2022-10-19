fe9e14f4e204edb501d17a2da4af2696252ded77
abdelaaziz jcweb <islamabdelaaziz@gmail.com>
2022-10-19 23:24:07 +0100

    ajouter page cours

---
+++ src/Controller/Frontend/CoursController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Frontend;
+use App\Entity\Cours;
+use App\Entity\Semestre;
+use App\Form\SemestreType;
+use App\Repository\CoursRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CoursController extends AbstractController
+{
+    #[Route('/cours/{id}', name: 'app_cours_detail')]
+    public function detailCours(Request $request,Cours $cours,CoursRepository $coursRepository ): Response
+    {
+        return $this->render('frontend/detail_cours.html.twig',array('cours'=>$cours));
+    }
+
+    #[Route('/semestre/{id}', name: 'app_semestre')]
+    public function coursBySemestre(Request $request,Semestre $semestre,CoursRepository $coursRepository ): Response
+    {
+        return $this->render('frontend/cours.html.twig',array('semestre'=>$semestre,'cours'=>$semestre->getCours()));
+    }
+
+}
